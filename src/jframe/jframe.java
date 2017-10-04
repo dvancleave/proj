a3677813b1bedb795eae01144aab9a012b8eeb2e
dvancleave <danielv@cs.uoregon.edu>
2017-10-03 22:04:21 -0700

    dsfdf

---
+++ src/jframe/jframe.java
@@ -0,0 +1,33 @@
+package jframe;
+
+import javax.swing.*;
+import java.awt.*;
+//import java.awt.event.*;
+
+public class jframe {
+	
+	private static void createAndShowGUI() {
+		JFrame frame = new JFrame("Address Book");
+		
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		JLabel emptyLabel = new JLabel("");
+		emptyLabel.setPreferredSize(new Dimension(500, 100));
+		//hjhgj
+		
+		frame.getContentPane().add(emptyLabel, BorderLayout.CENTER);
+		
+		frame.pack();
+		frame.pack();
+		frame.setVisible(true);
+	}
+	
+	public static void main(String[] args) {
+		javax.swing.SwingUtilities.invokeLater(new Runnable() {
+			public void run() {
+				createAndShowGUI();
+			}
+		});
+	}
+	
+	
+}
